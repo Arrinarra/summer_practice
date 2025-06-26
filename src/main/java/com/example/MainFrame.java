d71baf1de8dbc5991052a43c38e7d0c78fa94872
Arrinarra <156569304+Arrinarra@users.noreply.github.com>
2025-06-26 17:49:23 +0700

    добавление меню(изображение)

--- src/main/java/com/example/MainFrame.java
+++ src/main/java/com/example/MainFrame.java
@@ -1,5 +1,7 @@
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /*
  * нужно создать окно, в котором будет наше изображение. Для этого нужно наш класс
@@ -11,5 +13,14 @@ public class MainFrame extends JFrame {
 static {
     String nativePath = System.getProperty("user.dir") + "/native";
     System.load(nativePath + "/opencv_java455.dll"); // подключение OpenCV
+}
+private void initUI(){
+// Менюшечка
+JMenuBar menuBar = new JMenuBar();
+JMenu fileMenu = new JMenu("File");
+JMenuItem openItem = new JMenuItem("Open image");
+JMenuItem webcamItem = new JMenuItem("Webcam shoot");
+fileMenu.add(openItem);
+fileMenu.add(webcamItem);
 }
 }
