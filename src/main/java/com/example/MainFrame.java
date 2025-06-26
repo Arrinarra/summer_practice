a82cca1252bc8f3bcedb52675c6d679b9634c13a
Arrinarra <156569304+Arrinarra@users.noreply.github.com>
2025-06-26 17:25:30 +0700

    Просто первые строчки кода

--- src/main/java/com/example/MainFrame.java
+++ src/main/java/com/example/MainFrame.java
@@ -1,3 +1,15 @@
-public class MainFrame {
-    
+
+import javax.swing.JFrame;
+
+/*
+ * нужно создать окно, в котором будет наше изображение. Для этого нужно наш класс
+ * MainFrame расширить с помощью JFrame(если кратко, класс JFrame позволяет создавать окна).
+ */
+
+
+public class MainFrame extends JFrame {
+static {
+    String nativePath = System.getProperty("user.dir") + "/native";
+    System.load(nativePath + "/opencv_java455.dll"); // подключение OpenCV
+}
 }
