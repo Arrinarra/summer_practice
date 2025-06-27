77d160bd76d94259ce54ffaadaed5c9d76a265d8
Arrinarra <156569304+Arrinarra@users.noreply.github.com>
2025-06-27 15:29:18 +0700

    дополнена логика выбора действий главного меню

--- src/main/java/com/example/MainFrame.java
+++ src/main/java/com/example/MainFrame.java
@@ -1,9 +1,12 @@
 
+import java.awt.BorderLayout;
 import java.awt.MenuBar;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
 
 /*
  * нужно создать окно, в котором будет наше изображение. Для этого нужно наш класс
@@ -16,6 +19,15 @@ static {
     String nativePath = System.getProperty("user.dir") + "/native";
     System.load(nativePath + "/opencv_java455.dll"); // подключение OpenCV
 }
+
+// Экран(основной)
+public MainFrame() {
+    setTitle("Image Processor");
+    setSize(800,600);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    initUI();
+}
+
 private void initUI(){
 // Менюшечка(изображение)
 JMenuBar menuBar = new JMenuBar();
@@ -46,4 +58,33 @@ menuBar.add(filemenu);
 menuBar.add(channelMenu);
 menuBar.add(operationMenu);
 setJmenuBar(menuBar);
+
+// Панель изображения (экран -> панель(типа матрешки))
+imageLabel = new JLabel();
+imageLabel.setHorizontalAlignment(JLabel.CENTER); // выравнивание по центру
+add(new JScrollPane(imageLabel), BorderLayout.CENTER); // компонент помещается в прокручиваемую панель,
+// панель добавляется в центр главного окна
+ /*
+  * JScrollPane обеспечивает полосы прокрутки, если изображение больше окна
+  * BorderLayout.CENTER область в центре окна, которая растягивается при изменении размеров
+  */
+
+// обработчики событий 
+/*
+ * Чтобы сказать программе: "Когда жмается эта кнопка делай вот это"
+ */
+openItem.addActionListener(e -> openImage());
+webcamItem.addActionListener(e -> captureFromwebcam());
+
+// обработка цветовых каналов
+redItem.addActionListener(e -> showChannel(2)); // OpenCV BGR порядок
+greenItem.addActionListener(e -> showChannel(1));
+blueItem.addActionListener(e -> showChannel(0));
+
+// обработка операций варианта 10
+sharpenItem.addActionListener(e -> sharpemImage());
+rotateItem.addActionListener(e -> rotateImage());
+drawLineItem.addActionListener(e -> drawLine());
+
+
 }
